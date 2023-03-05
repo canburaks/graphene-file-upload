94468c46100b09b6bc9085577138ff145794e7d6
Can Burak S <canburak@msn.com>
2023-03-06 01:08:47 +0300

    generate function was added
    generateFileUploadGraphQLView function was added. It takes a GraphQL view and returns the original class

--- graphene_file_upload/django/__init__.py
+++ graphene_file_upload/django/__init__.py
@@ -20,3 +20,21 @@ class FileUploadGraphQLView(GraphQLView):
                 request.FILES
             )
         return super(FileUploadGraphQLView, self).parse_body(request)
+
+
+def generateFileUploadGraphQLView(view):
+    return class FileUploadGraphQLView(view):
+    """Handles multipart/form-data content type in django views"""
+
+    def parse_body(self, request):
+        """Handle multipart request spec for multipart/form-data"""
+        content_type = self.get_content_type(request)
+        if content_type == 'multipart/form-data' and 'operations' in request.POST:
+            operations = json.loads(request.POST.get('operations', '{}'))
+            files_map = json.loads(request.POST.get('map', '{}'))
+            return place_files_in_operations(
+                operations,
+                files_map,
+                request.FILES
+            )
+        return super(FileUploadGraphQLView, self).parse_body(request)
