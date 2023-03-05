c4418df7fa48d187a87a5fd65f5e816a22f5ceb0
Can Burak S <canburak@msn.com>
2023-03-06 01:18:57 +0300

    syntax fix 2

--- graphene_file_upload/django/__init__.py
+++ graphene_file_upload/django/__init__.py
@@ -24,17 +24,16 @@ class FileUploadGraphQLView(GraphQLView):
 
 def generateFileUploadGraphQLView(view):
     class FileUploadGraphQLView(view):
-    """Handles multipart/form-data content type in django views"""
-
-    def parse_body(self, request):
-        """Handle multipart request spec for multipart/form-data"""
-        content_type = self.get_content_type(request)
-        if content_type == 'multipart/form-data' and 'operations' in request.POST:
-            operations = json.loads(request.POST.get('operations', '{}'))
-            files_map = json.loads(request.POST.get('map', '{}'))
-            return place_files_in_operations(
-                operations,
-                files_map,
-                request.FILES
-            )
-        return super(FileUploadGraphQLView, self).parse_body(request)
+        """Handles multipart/form-data content type in django views"""
+        def parse_body(self, request):
+            """Handle multipart request spec for multipart/form-data"""
+            content_type = self.get_content_type(request)
+            if content_type == 'multipart/form-data' and 'operations' in request.POST:
+                operations = json.loads(request.POST.get('operations', '{}'))
+                files_map = json.loads(request.POST.get('map', '{}'))
+                return place_files_in_operations(
+                    operations,
+                    files_map,
+                    request.FILES
+                )
+            return super(FileUploadGraphQLView, self).parse_body(request)
